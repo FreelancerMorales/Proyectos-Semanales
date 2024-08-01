ec61f7ace69c060a72b739eb0b2a86ebaf081c93
Antonio Morales <camoralesc52@gmail.com>
2024-08-01 15:44:39 -0600

    Focus and Click in Btn with input

--- HTML with Api/scripts/animation.js
+++ HTML with Api/scripts/animation.js
@@ -11,3 +11,14 @@ function hideLoading() {
     const loading = document.getElementById('loading');
     loading.classList.remove('visible');
 }
+
+// Enter a Click
+const input = document.getElementById('city-input');
+const button = document.getElementById('search-button');
+
+input.addEventListener('keydown', function(event) {
+if (event.key === 'Enter') {
+    button.focus();
+    button.click(); // Activa el botón
+}
+});
