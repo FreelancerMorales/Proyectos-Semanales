d858e351fc3e1e5fc93c3d9ececb51480b1748af
Antonio Morales <camoralesc52@gmail.com>
2024-08-04 17:02:41 -0600

    Estable Carrusel

--- script/main.js
+++ script/main.js
@@ -1,3 +1,23 @@
 function climaCity(){
     window.location.href = "ClimaCity/index.html";
 }
+
+function imgRight() {
+    const img = document.querySelectorAll(".weather");
+    const imgLeft = img[0];
+    const imgRight = img[1];
+    imgLeft.classList.remove("visible");
+    imgLeft.classList.add("invisible");
+    imgRight.classList.remove("invisible");
+    imgRight.classList.add("visible");
+}
+
+function imgLeft() {
+    const img = document.querySelectorAll(".weather");
+    const imgLeft = img[0];
+    const imgRight = img[1];
+    imgRight.classList.remove("visible");
+    imgRight.classList.add("invisible");
+    imgLeft.classList.remove("invisible");
+    imgLeft.classList.add("visible");
+}
